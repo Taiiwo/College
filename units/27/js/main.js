2029b691b8f92577ee79ab7ae1243142c8117e4d
Taiiwo Llort <tylerzacc@gmail.com>
2015-04-03 17:40:14 +0100

    The fade is real

--- units/27/js/main.js
+++ units/27/js/main.js
@@ -1,15 +1,31 @@
+// set up fullpage.js (slides)
 $('#fullpage').fullpage({
-	anchors: ['s1', 's2', 's3'],
-	sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+	anchors: ['s0', 's1', 's2', 's3', 's4', 's5'],
 	navigation: true,
 	navigationPosition: 'right',
-	navigationTooltips: ['1', '2', '3'],
-	slidesNavigation: true
+	navigationTooltips: [	'Intro', 'Languages',
+												'Processing', 'Functionality',
+												'Features', 'Accessibility'],
+	slidesNavigation: true,
+	onLeave: function(anchor, index){
+		$('h1, h2, h3, h4, h5, p, span, li')
+		.animate({
+			opacity: 0
+		}, 150)
+			.animate({
+				opacity: 1
+			}, 800)
+	}
 });
+
+// set colours for page backgrounds
 var colours = [
 	Trianglify.colorbrewer.YlGn,
 	Trianglify.colorbrewer.PuOr,
-	Trianglify.colorbrewer.PuBu
+	Trianglify.colorbrewer.PuBu,
+	Trianglify.colorbrewer.RdYlGn,
+	Trianglify.colorbrewer.RdPu,
+	Trianglify.colorbrewer.Spectral
 ];
 $.each(colours, function(index, value){
 	var t = new Trianglify({
@@ -19,20 +35,6 @@ $.each(colours, function(index, value){
 	var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
 	$('#section' + index).css('background-image', pattern.dataUrl);
 });
-/*
-var t = new Trianglify({
-	x_gradient: Trianglify.colorbrewer.OrRd[8],
-	y_gradient: Trianglify.colorbrewer.OrRd[4]
-});
-var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
-$('#section1').css('background-image', pattern.dataUrl);
-var t = new Trianglify({
-	x_gradient: Trianglify.colorbrewer.PuBu[8],
-	y_gradient: Trianglify.colorbrewer.PuBu[4]
-});
-var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
-$('#section2').css('background-image', pattern.dataUrl);
-*/
 var sd = new Showdown.converter();
 $('xmd').each(function(index, value) {
 	value = $(value);
