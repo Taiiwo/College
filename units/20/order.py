fbb653a1d8c05d0676416fd8c6300bca117b5e97
Taiiwo Llort <tylerzacc@gmail.com>
2015-07-14 03:33:00 +0100

    implement the form in HTML

--- units/20/order.py
+++ units/20/order.py
@@ -1,4 +1,10 @@
 import os
 def index(req):
-	page = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'order.html')).read()
-	return page
+    # serve order.html instead
+    page = open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), 'order.html')
+    ).read()
+    return page
+def order(req, name, email, details):
+    r = "Thanks for ordering, %s" % (name)
+    return r
